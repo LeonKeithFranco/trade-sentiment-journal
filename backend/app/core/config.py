a04692555aa2b582364acb9d3cdf803da8ee267e
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-19 12:00:08 -0700

    feat(backend): add config related files

---
+++ backend/app/core/config.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class _Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=[
+            Path(__file__).parent.parent.parent.parent / ".env",
+            Path(__file__).parent.parent.parent / ".env",
+        ],
+        env_ignore_empty=True,
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=False,
+    )
+
+    postgres_user: str
+    postgres_db: str
