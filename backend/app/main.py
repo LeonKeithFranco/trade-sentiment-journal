3ace4121d6c30361bd4566c4cddc93b798f89ae1
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-19 15:46:52 -0700

    feat(backend): add logic to health check endpoint to test the database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from sqlalchemy import text
 
 from app.core.lifespan import lifespan
+from app.database.exceptions import DatabaseError
+from app.database.session import DbDependency
 
 app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/health")
-async def health_check() -> dict[str, str]:
+async def health_check(db: DbDependency) -> dict[str, str]:
+    try:
+        await db.execute(text("SELECT 1"))
+    except Exception as e:
+        raise DatabaseError("Could not connect to database") from e
+
     return {"status": "ok"}
