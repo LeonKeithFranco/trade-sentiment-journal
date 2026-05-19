362126fb541abd79967af56a62f717629249692c
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-19 14:52:26 -0700

    refactor(backend,database): add common fields to base class

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,5 +1,19 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    pass
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+    )
+    created_on: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
+    updated_on: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
