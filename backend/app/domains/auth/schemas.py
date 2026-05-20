c8571f1ec155458587f7d6f777fa9264a5beacfa
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-20 14:28:55 -0700

    feat(backend,schemas): add password validation

--- backend/app/domains/auth/schemas.py
+++ backend/app/domains/auth/schemas.py
@@ -1,7 +1,11 @@
+import re
 import uuid
 from datetime import datetime
 
-from pydantic import BaseModel, ConfigDict, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
+
+_ALLOWED_SYMBOLS = r"!@#$%^&*()_+\-="
+_DISALLOWED_SYMBOLS = r"\\\'\"<>;[\]|`~"
 
 
 class UserBase(BaseModel):
@@ -12,6 +16,24 @@ class UserRegisterRequest(UserBase):
     password: str = Field(
         min_length=12,
     )
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, v: str) -> str:
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Password must contain at least one lowercase latter")
+        if not re.search(r"[0-9]", v):
+            raise ValueError("Password must contain at least one number")
+        if not re.search(rf"[{_ALLOWED_SYMBOLS}]", v):
+            raise ValueError(
+                f"Password must contain at least one of: {_ALLOWED_SYMBOLS}"
+            )
+        if re.search(rf"[{_DISALLOWED_SYMBOLS}]", v):
+            raise ValueError(f"Password can't contain any of: {_DISALLOWED_SYMBOLS}")
+
+        return v
 
 
 class UserResponse(UserBase):
