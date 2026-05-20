5a637c0f38f232e1a05534a1e4b707ba4d270521
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-19 20:06:51 -0700

    feat(backend,database): add user sqlalchemy model

---
+++ backend/app/auth/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class User(Base):
+    email: Mapped[str] = mapped_column(
+        String(256),
+        unique=True,
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(256),
+    )
