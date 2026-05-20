7281608ff6ba0c70bd06e3c053bef86e54da1f6f
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-20 09:29:31 -0700

    feat(backend,auth): add logic to get user by email from db

--- backend/app/auth/repository.py
+++ backend/app/auth/repository.py
@@ -1,7 +1,19 @@
+from sqlalchemy import select
+
+from app.auth.models import User
 from app.database import DbDependency
 from app.database.repository import Repository
+
+type MaybeUser = User | None
 
 
 class AuthRepository(Repository):
     def __init__(self, db: DbDependency) -> None:
         super().__init__(db)
+
+    async def get_user_by_email(self, email: str) -> MaybeUser:
+        query = select(User).where(User.email == email)
+        results = await self.db.execute(query)
+        user = results.scalar_one_or_none()
+
+        return user
