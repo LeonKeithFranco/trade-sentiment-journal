9655a1ffa96e4636eb6039e52a5f1bd724548042
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-20 09:47:21 -0700

    feat(backend,auth): add logic to register user via auth service layer

--- backend/app/auth/service.py
+++ backend/app/auth/service.py
@@ -2,9 +2,32 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from app.auth.exceptions import UserAlreadyExists
+from app.auth.models import User
+from app.auth.repository import AuthRepoDependency
+from app.auth.schemas import UserRegisterRequest, UserResponse
+from app.security import hash_password
+
 
 class AuthService:
-    pass
+    async def register(
+        self, auth_repo: AuthRepoDependency, user_register_info: UserRegisterRequest
+    ) -> UserResponse:
+        existing_user = await auth_repo.get_user_by_email(user_register_info.email)
+
+        if existing_user is None:
+            raise UserAlreadyExists(
+                f"User with email {user_register_info.email} already exists"
+            )
+
+        user = await auth_repo.insert_user(
+            email=user_register_info.email,
+            hashed_password=hash_password(user_register_info.password),
+        )
+
+        await auth_repo.commit()
+
+        return UserResponse.model_validate(user)
 
 
 AuthServiceDependency = Annotated[AuthService, Depends(AuthService)]
