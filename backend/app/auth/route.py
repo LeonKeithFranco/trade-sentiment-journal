7ba343be01341d77bbe37e1461aee8b49a716525
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-20 10:11:21 -0700

    feat(backend,auth): add register endpoint

--- backend/app/auth/route.py
+++ backend/app/auth/route.py
@@ -1,3 +1,13 @@
 from fastapi import APIRouter
 
+from app.auth.schemas import UserRegisterRequest, UserResponse
+from app.auth.service import AuthServiceDependency
+
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router.get("/register", response_model=UserResponse)
+async def register(
+    user_register_request: UserRegisterRequest, auth_service: AuthServiceDependency
+) -> UserResponse:
+    return await auth_service.register(user_register_request)
