2fce44281d65d13f4e5344588f3be909afba796c
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-21 16:41:19 -0700

    tests(backend,auth): add token tests

--- backend/tests/unit/test_security.py
+++ backend/tests/unit/test_security.py
@@ -1,13 +1,39 @@
+import time
+import uuid
+from datetime import UTC, datetime, timedelta
+from pprint import pp
+
+import jwt
 import pytest
-from app.security import hash_password, verify_password
+from app.core.config import get_settings
+from app.exceptions import InvalidAccessTokenError
+from app.security import (
+    create_access_token,
+    create_refresh_token,
+    decode_access_token,
+    hash_password,
+    verify_password,
+)
 from app.security.password import _pepper_password
+from app.security.token import _ALGORITHM
 from pwdlib.exceptions import UnknownHashError
 from pytest_mock import MockerFixture
+
+
+def _jwt_encode(payload: dict) -> str:
+    return jwt.encode(
+        payload, key=get_settings().security.token_secret, algorithm=_ALGORITHM
+    )
 
 
 @pytest.fixture(scope="session")
 def hashed_password(default_password: str) -> str:
     return hash_password(default_password)
+
+
+@pytest.fixture(scope="session")
+def fake_user_public_id() -> uuid.UUID:
+    return uuid.UUID("d5c7c282-b35a-4a7c-b361-c3aa7b4b1715")
 
 
 class TestPassword:
@@ -46,3 +72,87 @@ class TestPassword:
         mock_verify.assert_called_once_with(
             password=_pepper_password(default_password), hash=hashed_password
         )
+
+
+class TestAccessToken:
+    def test_create_and_decode_access_token(
+        self, fake_user_public_id: uuid.UUID
+    ) -> None:
+        access_token = create_access_token(fake_user_public_id)
+        decoded_user_id = decode_access_token(access_token)
+
+        assert decoded_user_id == str(fake_user_public_id)
+
+    def test_decode_expired_access_token(self, fake_user_public_id: uuid.UUID) -> None:
+        expired_payload = {
+            "sub": str(fake_user_public_id),
+            "type": "access",
+            "exp": datetime.now(UTC)
+            - timedelta(minutes=get_settings().security.access_token_expire_minutes),
+        }
+
+        token = _jwt_encode(expired_payload)
+
+        time.sleep(1)
+
+        with pytest.raises(InvalidAccessTokenError, match="Token has expired"):
+            decode_access_token(token)
+
+    def test_decode_wrong_type_access_token(
+        self, fake_user_public_id: uuid.UUID
+    ) -> None:
+        wrong_type_payload = {
+            "sub": str(fake_user_public_id),
+            "type": "not_access",
+            "exp": datetime.now(UTC) + timedelta(minutes=5),
+        }
+
+        token = _jwt_encode(wrong_type_payload)
+
+        with pytest.raises(InvalidAccessTokenError, match="Token type is incorrect"):
+            decode_access_token(token)
+
+    def test_decode_missing_sub_access_token(self) -> None:
+        wrong_type_payload = {
+            "type": "access",
+            "exp": datetime.now(UTC) + timedelta(minutes=5),
+        }
+
+        token = _jwt_encode(wrong_type_payload)
+
+        with pytest.raises(InvalidAccessTokenError, match="Token subject is missing"):
+            decode_access_token(token)
+
+    def test_decode_invalid_access_token(
+        self, mocker: MockerFixture, fake_user_public_id: uuid.UUID
+    ) -> None:
+        payload = {
+            "sub": str(fake_user_public_id),
+            "type": "access",
+            "exp": datetime.now(UTC)
+            + timedelta(minutes=get_settings().security.access_token_expire_minutes),
+            "iat": datetime.now(UTC),
+        }
+
+        token = _jwt_encode(payload)
+
+        mock_decode = mocker.patch(
+            "app.security.token.jwt.decode", side_effect=jwt.InvalidTokenError
+        )
+
+        with pytest.raises(InvalidAccessTokenError, match="Token is invalid"):
+            decode_access_token(token)
+
+        mock_decode.assert_called_once_with(
+            token, key=get_settings().security.token_secret, algorithms=[_ALGORITHM]
+        )
+
+
+class TestRefreshToken:
+    def test_create_refresh_token(self) -> None:
+        token, expire = create_refresh_token()
+        today = datetime.now(UTC)
+
+        assert (
+            expire.date() - today.date()
+        ).days == get_settings().security.refresh_token_expire_days
