29ba7949a74e9033fdccd01b4e01a24bb2ceb5f6
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-21 14:55:00 -0700

    feat(backend,auth): add endpoint for login

--- backend/app/domains/auth/route.py
+++ backend/app/domains/auth/route.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, status
 
-from app.domains.auth.schemas import UserRegisterRequest, UserResponse
+from app.domains.auth.schemas import (
+    TokenResponse,
+    UserLoginRequest,
+    UserRegisterRequest,
+    UserResponse,
+)
 from app.domains.auth.service import AuthServiceDependency
 
 router = APIRouter(prefix="/auth", tags=["auth"])
@@ -13,3 +18,10 @@ async def register(
     user_register_request: UserRegisterRequest, auth_service: AuthServiceDependency
 ) -> UserResponse:
     return await auth_service.register(user_register_request)
+
+
+@router.post("/login", response_model=TokenResponse)
+async def login(
+    user_login_request: UserLoginRequest, auth_service: AuthServiceDependency
+) -> TokenResponse:
+    return await auth_service.login(user_login_request)
