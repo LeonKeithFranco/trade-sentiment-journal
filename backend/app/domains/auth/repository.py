68c7ec28d2162e2dd38125d944fd54661d39801d
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-22 12:24:49 -0700

    feat(backend): add get fresh token method to repo

--- backend/app/domains/auth/repository.py
+++ backend/app/domains/auth/repository.py
@@ -4,12 +4,14 @@ from typing import Annotated
 
 from fastapi import Depends
 from sqlalchemy import ColumnElement, select
+from sqlalchemy.orm import selectinload
 
 from app.database import DbDependency
 from app.database.repository import Repository
 from app.domains.auth.models import RefreshToken, User
 
 type MaybeUser = User | None
+type MaybeRefreshToken = RefreshToken | None
 
 
 class UserRepository(Repository):
@@ -46,6 +48,16 @@ UserRepoDependency = Annotated[UserRepository, Depends(UserRepository)]
 class RefreshTokenRepository(Repository):
     def __init__(self, db: DbDependency) -> None:
         super().__init__(db)
+
+    async def get_refresh_token(self, token: str) -> MaybeRefreshToken:
+        query = (
+            select(RefreshToken)
+            .where(RefreshToken.token == token)
+            .options(selectinload(RefreshToken.user))
+        )
+        results = await self.db.execute(query)
+
+        return results.scalar_one_or_none()
 
     async def insert_refresh_token(
         self, user_id: int, refresh_token: str, expire: datetime
