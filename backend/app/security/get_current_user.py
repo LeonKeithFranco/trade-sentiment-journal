4c4919969885569561de35a029ede78f0cb9ceaf
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-22 09:25:07 -0700

    feat(backend,auth): add get current user dependency for route protection

---
+++ backend/app/security/get_current_user.py
@@ -0,0 +1,29 @@
+import uuid
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+
+from app.domains.auth.repository import UserRepoDependency
+from app.exceptions import UserInvalidCredentialsError
+from app.models import User
+from app.security import decode_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], user_repo: UserRepoDependency
+) -> User:
+    user_public_id = uuid.UUID(decode_access_token(token))
+    user = await user_repo.get_user_by_public_id(user_public_id)
+
+    if user is None:
+        raise UserInvalidCredentialsError(
+            f"User with public id {user_public_id} does not exist"
+        )
+
+    return user
+
+
+CurrentUserDependency = Annotated[User, Depends(get_current_user)]
