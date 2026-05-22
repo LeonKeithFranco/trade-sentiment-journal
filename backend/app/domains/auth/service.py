d0f40cfcdbe9681a3dca7777d450790ef3f4818a
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-22 12:26:33 -0700

    refactor+feat(backend,auth): refactor creating tokens into helper function and add refresh tokens method

--- backend/app/domains/auth/service.py
+++ backend/app/domains/auth/service.py
@@ -1,3 +1,5 @@
+import uuid
+from datetime import UTC, datetime
 from typing import Annotated
 
 from fastapi import Depends
@@ -13,11 +15,13 @@ from app.domains.auth.repository import (
     UserRepository,
 )
 from app.domains.auth.schemas import (
+    RefreshRequest,
     TokenResponse,
     UserLoginRequest,
     UserRegisterRequest,
     UserResponse,
 )
+from app.models import User
 from app.security import (
     create_access_token,
     create_refresh_token,
@@ -35,6 +39,16 @@ class AuthService:
     ) -> None:
         self.user_repo: UserRepository = user_repo
         self.refresh_token_repo: RefreshTokenRepository = refresh_token_repo
+
+    async def _create_new_tokens(self, user: User) -> tuple[str, str]:
+        access_token = create_access_token(user.public_id)
+
+        refresh_token_info = create_refresh_token()
+        refresh_token_record = await self.refresh_token_repo.insert_refresh_token(
+            user.id, *refresh_token_info
+        )
+
+        return access_token, refresh_token_record.token
 
     async def register(self, user_register_info: UserRegisterRequest) -> UserResponse:
         existing_user = await self.user_repo.get_user_by_email(user_register_info.email)
@@ -67,19 +81,30 @@ class AuthService:
         if not verify_password(user_login_info.password, existing_user.hashed_password):
             raise UserInvalidCredentialsError("Password is incorrect.")
 
-        access_token = create_access_token(existing_user.public_id)
+        access_token, refresh_token = await self._create_new_tokens(existing_user)
 
-        refresh_token_info = create_refresh_token()
-        refresh_token_record = await self.refresh_token_repo.insert_refresh_token(
-            existing_user.id, *refresh_token_info
+        await self.refresh_token_repo.commit()
+
+        return TokenResponse(access_token=access_token, refresh_token=refresh_token)
+
+    async def refresh_tokens(self, refresh_info: RefreshRequest) -> TokenResponse:
+        refresh_token_record = await self.refresh_token_repo.get_refresh_token(
+            refresh_info.refresh_token
+        )
+
+        if refresh_token_record is None:
+            raise UserInvalidCredentialsError("Invalid refresh token")
+
+        if refresh_token_record.expires_on < datetime.now(UTC):
+            raise UserInvalidCredentialsError("Refresh token is expired")
+
+        access_token, refresh_token = await self._create_new_tokens(
+            refresh_token_record.user
         )
 
         await self.refresh_token_repo.commit()
 
-        return TokenResponse(
-            access_token=access_token,
-            refresh_token=refresh_token_record.token,
-        )
+        return TokenResponse(access_token=access_token, refresh_token=refresh_token)
 
 
 AuthServiceDependency = Annotated[AuthService, Depends(AuthService)]
