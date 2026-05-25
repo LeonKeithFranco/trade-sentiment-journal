43a478770564b9abedc67c5981c5f2b96cb72380
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-25 14:16:47 -0700

    feat(backend,trade): add service layer method for creating trades

--- backend/app/domains/trade/service.py
+++ backend/app/domains/trade/service.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 
 from app.domains.auth.repository import UserRepoDependency, UserRepository
 from app.domains.trade.repository import TradeRepoDependency, TradeRepository
+from app.domains.trade.schemas import TradeRequest, TradeResponse
 
 
 class TradeService:
@@ -14,6 +15,11 @@ class TradeService:
     ) -> None:
         self.user_repo: UserRepository = user_repo
         self.trade_repo: TradeRepository = trade_repo
+
+    async def create(self, trade_info: TradeRequest, user_id: int) -> TradeResponse:
+        new_trade = await self.trade_repo.insert_trade(trade_info, user_id)
+
+        return TradeResponse.model_validate(new_trade)
 
 
 TradeServiceDependency = Annotated[TradeService, Depends(TradeService)]
