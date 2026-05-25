cbaaa2c16597bd7ce9cda96022466c2ae0466c1b
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-25 15:53:23 -0700

    refactor(backend,trades): remove unused user repo in trade service

--- backend/app/domains/trades/service.py
+++ backend/app/domains/trades/service.py
@@ -2,7 +2,6 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from app.domains.auth.repository import UserRepoDependency, UserRepository
 from app.domains.trades.repository import TradeRepoDependency, TradeRepository
 from app.domains.trades.schemas import TradeRequest, TradeResponse
 
@@ -10,10 +9,8 @@ from app.domains.trades.schemas import TradeRequest, TradeResponse
 class TradeService:
     def __init__(
         self,
-        user_repo: UserRepoDependency,
         trade_repo: TradeRepoDependency,
     ) -> None:
-        self.user_repo: UserRepository = user_repo
         self.trade_repo: TradeRepository = trade_repo
 
     async def create(self, trade_info: TradeRequest, user_id: int) -> TradeResponse:
