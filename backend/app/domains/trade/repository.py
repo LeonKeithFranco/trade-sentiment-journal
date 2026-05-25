1c4a91102aa2e1fb47d86a8e398dcfcada6eb7d9
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-25 14:15:46 -0700

    feat(backend,trade): add repo method to insert trade

--- backend/app/domains/trade/repository.py
+++ backend/app/domains/trade/repository.py
@@ -4,11 +4,23 @@ from fastapi import Depends
 
 from app.database import DbDependency
 from app.database.repository import Repository
+from app.domains.trade.schemas import TradeRequest
+from app.models import Trade
 
 
 class TradeRepository(Repository):
     def __init__(self, db: DbDependency) -> None:
         super().__init__(db)
+
+    async def insert_trade(self, trade_info: TradeRequest, user_id: int) -> Trade:
+        trade = Trade(**trade_info.model_dump(exclude_unset=True))
+        trade.user_id = user_id
+
+        self.db.add(trade)
+        await self.db.flush()
+        await self.db.refresh(trade)
+
+        return trade
 
 
 TradeRepoDependency = Annotated[TradeRepository, Depends(TradeRepository)]
