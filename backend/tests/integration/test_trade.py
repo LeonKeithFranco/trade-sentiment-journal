a2350a8365101af0b5b2f4da7df06116931bc796
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-25 15:35:38 -0700

    tests(backend,trade): add failure most tests for creating trades

--- backend/tests/integration/test_trade.py
+++ backend/tests/integration/test_trade.py
@@ -1,9 +1,15 @@
+import uuid
 from datetime import UTC, datetime, timedelta
 from decimal import Decimal
+from pprint import pp
 
 import pytest
+from app.models import Trade
 from fastapi import status
 from fastapi.testclient import TestClient
+from sqlalchemy import Engine, insert, select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 _OPENED_AT = datetime.now(UTC).isoformat().removesuffix("+00:00") + "Z"
 _CLOSED_AT = (
@@ -95,3 +101,196 @@ class TestCreate:
         assert "public_id" in data
         assert "created_on" in data
         assert "updated_on" in data
+
+    def test_add_trade_to_non_existent_user(self, client: TestClient) -> None:
+        payload = {
+            "ticker": "MAPPL",
+            "direction": "LONG",
+            "position_size": 3.33,
+            "entry_price": 50.51,
+            "exit_price": None,
+            "opened_at": _OPENED_AT,
+            "closed_at": None,
+        }
+
+        response = client.post(
+            "/trade",
+            headers={"Authorization": f"Bearer {uuid.uuid4()}"},
+            json=payload,
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+        detail = response.json()["detail"]
+
+        assert detail == "Token is invalid"
+
+    @pytest.mark.parametrize(
+        "payload",
+        [
+            {
+                "ticker": "MAPPL",
+                "direction": "LONG",
+                "position_size": 3.33,
+                "entry_price": 50.51,
+                "exit_price": 6.7,
+                "opened_at": _OPENED_AT,
+                "closed_at": None,
+            },
+            {
+                "ticker": "GNG",
+                "direction": "SHORT",
+                "position_size": 1.01,
+                "entry_price": 34.56,
+                "exit_price": None,
+                "opened_at": _OPENED_AT,
+                "closed_at": _CLOSED_AT,
+            },
+        ],
+    )
+    def test_closed_at_or_exit_price_one_none(
+        self, client: TestClient, access_token: str, payload: dict
+    ) -> None:
+        response = client.post(
+            "/trade",
+            headers={"Authorization": f"Bearer {access_token}"},
+            json=payload,
+        )
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+        detail = response.json()["detail"][0]
+
+        assert detail == {
+            "type": "value_error",
+            "loc": ["body"],
+            "msg": "Value error, exit_price and closed_at must both be None or must both "
+            "have values",
+            "input": payload,
+            "ctx": {"error": {}},
+        }
+
+    def test_closed_at_before_opened_at(
+        self, client: TestClient, access_token: str
+    ) -> None:
+        payload = {
+            "ticker": "BSLA",
+            "direction": "LONG",
+            "position_size": 1,
+            "entry_price": 1,
+            "exit_price": 2,
+            "opened_at": _CLOSED_AT,
+            "closed_at": _OPENED_AT,
+        }
+
+        response = client.post(
+            "/trade",
+            headers={"Authorization": f"Bearer {access_token}"},
+            json=payload,
+        )
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+        detail = response.json()["detail"][0]
+
+        assert detail == {
+            "type": "value_error",
+            "loc": ["body"],
+            "msg": "Value error, closed_at should be later than opened_at",
+            "input": payload,
+            "ctx": {"error": {}},
+        }
+
+    def test_direction_constraint(
+        self,
+        db_engine: Engine,
+        access_token: str,  # here to make sure a use is in the db
+    ) -> None:
+        with pytest.raises(IntegrityError):
+            with db_engine.begin() as conn:
+                query = insert(Trade).values(
+                    {
+                        "user_id": 1,
+                        "ticker": "MAPPL",
+                        "_direction": "FAKE",
+                        "position_size": 3.33,
+                        "entry_price": 50.51,
+                        "exit_price": None,
+                        "opened_at": _OPENED_AT,
+                        "closed_at": None,
+                    }
+                )
+
+                conn.execute(query)
+
+    def test_closed_at_after_opened_at_constraint(
+        self,
+        db_engine: Engine,
+        access_token: str,  # here to make sure a use is in the db
+    ) -> None:
+        with pytest.raises(IntegrityError):
+            with Session(db_engine) as session:
+                query = insert(Trade).values(
+                    {
+                        "user_id": 1,
+                        "ticker": "MAPPL",
+                        "_direction": "FAKE",
+                        "position_size": 3.33,
+                        "entry_price": 50.51,
+                        "exit_price": 100.11,
+                        "opened_at": _CLOSED_AT,
+                        "closed_at": _OPENED_AT,
+                    }
+                )
+
+                session.execute(query)
+
+    @pytest.mark.parametrize(
+        "values,pnl",
+        [
+            (
+                {
+                    "ticker": "BSLA",
+                    "_direction": "LONG",
+                    "position_size": 1,
+                    "entry_price": 1,
+                    "exit_price": 2,
+                    "opened_at": _OPENED_AT,
+                    "closed_at": _CLOSED_AT,
+                    "user_id": 1,
+                },
+                1,
+            ),
+            (
+                {
+                    "ticker": "DRK",
+                    "_direction": "SHORT",
+                    "position_size": 1,
+                    "entry_price": 1,
+                    "exit_price": 2,
+                    "opened_at": _OPENED_AT,
+                    "closed_at": _CLOSED_AT,
+                    "user_id": 1,
+                },
+                -1,
+            ),
+        ],
+    )
+    def test_calculate_pnl_event(
+        self,
+        db_engine: Engine,
+        access_token: str,  # here to make sure a use is in the db
+        values: dict,
+        pnl: Decimal,
+    ) -> None:
+        with Session(db_engine) as session:
+            trade = Trade(**values)
+
+            session.add(trade)
+            session.commit()
+
+            session.refresh(trade)
+
+            assert trade is not None
+            assert trade.profit_and_loss is not None
+            assert str(trade.profit_and_loss) == f"{pnl:.2f}"
