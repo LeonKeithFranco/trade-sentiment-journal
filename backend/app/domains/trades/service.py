2408f2f91e29420bbd1c2148eb8694f974eb8bb2
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-26 15:39:18 -0700

    feat(backend,trade): add service layer method to update trade

--- backend/app/domains/trades/service.py
+++ backend/app/domains/trades/service.py
@@ -5,7 +5,12 @@ from fastapi import Depends
 
 from app.domains.trades.exceptions import TradeDoesNotExistError
 from app.domains.trades.repository import TradeRepoDependency, TradeRepository
-from app.domains.trades.schemas import TradeCreateRequest, TradeResponse
+from app.domains.trades.schemas import (
+    TradeCreateRequest,
+    TradeResponse,
+    TradeUpdateRequest,
+)
+from app.models import Trade
 
 
 class TradeService:
@@ -14,6 +19,18 @@ class TradeService:
         trade_repo: TradeRepoDependency,
     ) -> None:
         self.trade_repo: TradeRepository = trade_repo
+
+    async def _get_helper(self, trade_public_id: uuid.UUID, user_id: int) -> Trade:
+        trade = await self.trade_repo.get_trade_by_public_id_for_user(
+            trade_public_id, user_id
+        )
+
+        if trade is None:
+            raise TradeDoesNotExistError(
+                f"There is no trade with public_id {trade_public_id} for user with id {user_id}"
+            )
+
+        return trade
 
     async def create(
         self, trade_info: TradeCreateRequest, user_id: int
@@ -29,14 +46,7 @@ class TradeService:
         return TradeResponse.model_validate(new_trade)
 
     async def get(self, trade_public_id: uuid.UUID, user_id: int) -> TradeResponse:
-        trade = await self.trade_repo.get_trade_by_public_id_for_user(
-            trade_public_id, user_id
-        )
-
-        if trade is None:
-            raise TradeDoesNotExistError(
-                f"There is no trade with public_id {trade_public_id} for user with id {user_id}"
-            )
+        trade = await self._get_helper(trade_public_id, user_id)
 
         return TradeResponse.model_validate(trade)
 
@@ -63,6 +73,21 @@ class TradeService:
             )
 
         await self.trade_repo.commit()
+
+    async def update(
+        self,
+        trade_update_info: TradeUpdateRequest,
+        trade_public_id: uuid.UUID,
+        user_id: int,
+    ) -> TradeResponse:
+        trade = trade = await self._get_helper(trade_public_id, user_id)
+        trade_update_info_dict = trade_update_info.model_dump(exclude_unset=True)
+
+        await self.trade_repo.update_trade(trade, **trade_update_info_dict)
+
+        await self.trade_repo.commit()
+
+        return TradeResponse.model_validate(trade)
 
 
 TradeServiceDependency = Annotated[TradeService, Depends(TradeService)]
