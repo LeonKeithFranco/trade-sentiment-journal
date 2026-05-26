69d7534cac2680208d5f41579fa5d75c36901917
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-26 09:25:23 -0700

    feat(backend,trade): add get trade method to service layer

--- backend/app/domains/trades/service.py
+++ backend/app/domains/trades/service.py
@@ -1,7 +1,9 @@
+import uuid
 from typing import Annotated
 
 from fastapi import Depends
 
+from app.domains.trades.exceptions import TradeDoesNotExistError
 from app.domains.trades.repository import TradeRepoDependency, TradeRepository
 from app.domains.trades.schemas import TradeRequest, TradeResponse
 
@@ -23,6 +25,18 @@ class TradeService:
         await self.trade_repo.commit()
 
         return TradeResponse.model_validate(new_trade)
+
+    async def get(self, trade_public_id: uuid.UUID, user_id: int) -> TradeResponse:
+        trade = await self.trade_repo.get_trade_by_public_id_for_user(
+            trade_public_id, user_id
+        )
+
+        if trade is None:
+            raise TradeDoesNotExistError(
+                f"There is no trade with public_id {trade_public_id} for user with id {user_id}"
+            )
+
+        return TradeResponse.model_validate(trade)
 
 
 TradeServiceDependency = Annotated[TradeService, Depends(TradeService)]
