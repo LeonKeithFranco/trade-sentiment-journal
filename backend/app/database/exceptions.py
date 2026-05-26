f5efca4d36370925b2b0e876890cb6d4261a8856
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-26 16:50:36 -0700

    feat(backend): add database exception

--- backend/app/database/exceptions.py
+++ backend/app/database/exceptions.py
@@ -1,2 +1,6 @@
 class DatabaseError(RuntimeError):
     """There is an issue when trying to use the database."""
+
+
+class ClosedAtBeforeOpenedAtError(DatabaseError):
+    """There is an attempt to try to set closed_at datetime to before opened_at datetime and vice versa."""
