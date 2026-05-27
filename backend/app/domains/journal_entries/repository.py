d5f902c673f5592805aefa6299af2456b7a85333
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-27 13:16:39 -0700

    feat(backend,journal-entries): add get repo method

--- backend/app/domains/journal_entries/repository.py
+++ backend/app/domains/journal_entries/repository.py
@@ -1,10 +1,14 @@
+import uuid
 from typing import Annotated
 
 from fastapi import Depends
+from sqlalchemy import select
 
 from app.database import DbDependency
 from app.database.repository import Repository
 from app.models import JournalEntry
+
+type MaybeJournalEntry = JournalEntry | None
 
 
 class JournalEntryRepository(Repository):
@@ -24,6 +28,19 @@ class JournalEntryRepository(Repository):
         self.db.add(journal_entry)
         await self.db.flush()
         await self.db.refresh(journal_entry)
+
+        return journal_entry
+
+    async def get_journal_entry_by_public_id_for_user(
+        self, trade_public_id: uuid.UUID, user_id: int
+    ) -> MaybeJournalEntry:
+        query = (
+            select(JournalEntry)
+            .where(JournalEntry.public_id == trade_public_id)
+            .where(JournalEntry.user_id == user_id)
+        )
+        results = await self.db.execute(query)
+        journal_entry = results.scalar_one_or_none()
 
         return journal_entry
 
