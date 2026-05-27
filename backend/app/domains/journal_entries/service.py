13076e0597a6774c048436b3498b85ddef35dcc9
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-27 10:49:56 -0700

    feat(backend,journal-entries): add create journal entry method in service layer

--- backend/app/domains/journal_entries/service.py
+++ backend/app/domains/journal_entries/service.py
@@ -6,6 +6,10 @@ from app.domains.journal_entries.repository import (
     JournalEntryRepoDependency,
     JournalEntryRepository,
 )
+from app.domains.journal_entries.schemas import (
+    JournalEntryCreateRequest,
+    JournalEntryResponse,
+)
 
 
 class JournalEntryService:
@@ -14,6 +18,25 @@ class JournalEntryService:
         journal_entry_repo: JournalEntryRepoDependency,
     ) -> None:
         self.journal_entry_repo: JournalEntryRepository = journal_entry_repo
+
+    async def create(
+        self,
+        journal_entry_create_info: JournalEntryCreateRequest,
+        user_id: int,
+        trade_id: int,
+    ) -> JournalEntryResponse:
+        journal_entry_create_info_dict = journal_entry_create_info.model_dump(
+            exclude_unset=True
+        )
+        journal_entry_create_info_dict.pop("trade_public_id")
+
+        new_journal_entry = await self.journal_entry_repo.insert_journal_entry(
+            user_id=user_id, trade_id=trade_id, **journal_entry_create_info_dict
+        )
+
+        await self.journal_entry_repo.commit()
+
+        return JournalEntryResponse.model_validate(new_journal_entry)
 
 
 JournalEntryServiceDependency = Annotated[
