a240a662f7219015d46e260bf400df59bbb7d08e
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-27 10:47:04 -0700

    feat(backend,journal-entries): add journal create request and response schemas

--- backend/app/domains/journal_entries/schemas.py
+++ backend/app/domains/journal_entries/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field
+import uuid
+
+from pydantic import AwareDatetime, BaseModel, ConfigDict, Field
 
 from app.core.constants import MAX_TITLE_LENGTH
 
@@ -12,3 +14,17 @@ class JournalEntryBase(BaseModel):
     entry: str = Field(
         min_length=50,
     )
+
+
+class JournalEntryCreateRequest(JournalEntryBase):
+    trade_public_id: uuid.UUID
+
+
+class JournalEntryResponse(JournalEntryBase):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+    public_id: uuid.UUID
+    created_on: AwareDatetime
+    updated_on: AwareDatetime
