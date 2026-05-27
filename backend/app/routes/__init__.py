7f735bf228ba44fde85fe5dfccfcd5041db62fd7
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-27 11:23:04 -0700

    refactor(backend,journal-entries): fix typo

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,7 @@
 from app.domains.auth.route import router as auth_router
-from app.domains.journal_entries.route import router as jounal_entries_router
+from app.domains.journal_entries.route import router as journal_entries_router
 from app.domains.trades.route import router as trades_router
 
-routers = [auth_router, trades_router, jounal_entries_router]
+routers = [auth_router, trades_router, journal_entries_router]
 
 __all__ = ["routers"]
