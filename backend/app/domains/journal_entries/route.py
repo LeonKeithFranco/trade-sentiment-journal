04d4bedce3dad066d9857238a1fac97ead9c6924
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-27 10:52:26 -0700

    feat(backend,journal-entries): add create journal entry endpoint

--- backend/app/domains/journal_entries/route.py
+++ backend/app/domains/journal_entries/route.py
@@ -1,3 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+
+from app.domains.journal_entries.schemas import (
+    JournalEntryCreateRequest,
+    JournalEntryResponse,
+)
+from app.domains.journal_entries.service import JournalEntryServiceDependency
+from app.domains.trades.service import TradeServiceDependency
+from app.security import CurrentUserDependency
 
 router = APIRouter(prefix="/journal-entries", tags=["journal-entries"])
+
+
+@router.post(
+    "", response_model=JournalEntryResponse, status_code=status.HTTP_201_CREATED
+)
+async def create(
+    journal_entry_create_request: JournalEntryCreateRequest,
+    current_user: CurrentUserDependency,
+    trade_service: TradeServiceDependency,
+    journal_entry_serivce: JournalEntryServiceDependency,
+) -> JournalEntryResponse:
+    trade = await trade_service.get_trade(
+        journal_entry_create_request.trade_public_id, current_user.id
+    )
+
+    return await journal_entry_serivce.create(
+        journal_entry_create_request, current_user.id, trade.id
+    )
