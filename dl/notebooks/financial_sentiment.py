e51adb7d02b064d09c3ce4deb5a31a58d28fd8e2
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-28 12:39:30 -0700

    feat(dl): add preprocessing and caching the preprocessed data to disk

--- dl/notebooks/financial_sentiment.py
+++ dl/notebooks/financial_sentiment.py
@@ -8,19 +8,50 @@ app = marimo.App(width="medium")
 def _():
     from datasets import load_dataset, load_from_disk
     from utils import constants
+    from utils.preprocess import process_sentence
 
-    return constants, load_dataset, load_from_disk
+    return constants, load_dataset, load_from_disk, process_sentence
 
 
 @app.cell
-def _(constants, load_dataset, load_from_disk):
-    if constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH.exists():
-        dataset = load_from_disk(constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH)
-    else:
-        dataset = load_dataset("lmassaron/FinancialPhraseBank")
-        dataset.save_to_disk(constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH)
+def _(process_sentence):
+    from datasets.arrow_dataset import Dataset
+    from datasets import DatasetDict
 
-    dataset
+
+    def process_dataset_sentences(dataset: Dataset) -> Dataset:
+        return dataset.map(
+            lambda data: {"sentence": process_sentence(data["sentence"])}
+        )
+
+
+    def process_full_dataset_sentences(full_dataset: DatasetDict) -> DatasetDict:
+        return DatasetDict(
+            {
+                split: process_dataset_sentences(data)
+                for split, data in full_dataset.items()
+            }
+        )
+
+    return (process_full_dataset_sentences,)
+
+
+@app.cell
+def _(constants, load_dataset, load_from_disk, process_full_dataset_sentences):
+    if constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH.exists():
+        full_dataset = load_from_disk(constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH)
+    else:
+        full_dataset = load_dataset("lmassaron/FinancialPhraseBank")
+        full_dataset = process_full_dataset_sentences(full_dataset)
+        full_dataset.save_to_disk(constants.FINANCIAL_PHRASE_BANK_FOLDER_PATH)
+
+    full_dataset
+    return (full_dataset,)
+
+
+@app.cell
+def _(full_dataset):
+    full_dataset['train'][100]['sentence']
     return
 
 
