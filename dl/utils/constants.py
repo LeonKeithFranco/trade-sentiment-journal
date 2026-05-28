a1c356ac6396fcf58717acca025aaf550f986b1b
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-28 09:39:53 -0700

    feat(dl): add path constant for phrase dataset

--- dl/utils/constants.py
+++ dl/utils/constants.py
@@ -3,3 +3,4 @@ from pathlib import Path
 _DATA_FOLDER_PATH = Path(__file__).parent.parent / "data"
 
 GLOVE_EMBEDDINGS_FILE_PATH = _DATA_FOLDER_PATH / "glove.6B.100d.txt"
+FINANCIAL_PHRASE_BANK_FOLDER_PATH = _DATA_FOLDER_PATH / "financial_phrase_bank"
