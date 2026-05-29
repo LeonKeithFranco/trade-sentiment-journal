b6df5203ab3803436b7d7fcc6a39ba7feb50486a
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-29 08:35:57 -0700

    chore(dl): add updated constant to git tracking

--- dl/utils/constants.py
+++ dl/utils/constants.py
@@ -4,3 +4,4 @@ _DATA_FOLDER_PATH = Path(__file__).parent.parent / "data"
 
 GLOVE_EMBEDDINGS_FILE_PATH = _DATA_FOLDER_PATH / "glove.6B.100d.txt"
 FINANCIAL_PHRASE_BANK_FOLDER_PATH = _DATA_FOLDER_PATH / "financial_phrase_bank"
+VOCAB_MAPPING_FILE_PATH = _DATA_FOLDER_PATH / "vocab_mapping.json"
