338d80b139a62a1493459ccd40b07f4d64978f39
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-02 10:41:54 -0700

    feat(dl): add model class

--- dl/notebooks/model.py
+++ dl/notebooks/model.py
@@ -8,11 +8,25 @@ app = marimo.App(width="medium")
 def _():
     import torch
     from torch.utils.data import DataLoader
+    from torch.nn.utils.rnn import pack_padded_sequence
+    from torch import nn
     import datasets
     from utils.get_dataset import get_dataset
     from utils.preprocess import map_sentence
+    import numpy as np
+    from utils import constants
 
-    return DataLoader, datasets, get_dataset, map_sentence, torch
+    return (
+        DataLoader,
+        constants,
+        datasets,
+        get_dataset,
+        map_sentence,
+        nn,
+        np,
+        pack_padded_sequence,
+        torch,
+    )
 
 
 @app.cell
@@ -96,21 +110,82 @@ def _(torch):
 
 @app.cell
 def _(DataLoader, collate, train_dataset):
-    train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=32, shuffle=True, collate_fn=collate
+    )
     sample_batch = next(iter(train_dataloader))
     sample_batch[0].shape, sample_batch[1].shape, sample_batch[2].shape
+    return (sample_batch,)
+
+
+@app.cell
+def _(DataLoader, collate, test_dataset, val_dataset):
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=32, shuffle=False, collate_fn=collate
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=32, shuffle=False, collate_fn=collate
+    )
     return
 
 
 @app.cell
-def _(DataLoader, collate, val_dataset):
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False, collate_fn=collate)
+def _(constants, np, torch):
+    matrix_embedding = torch.tensor(np.load(constants.MATRIX_EMBEDDINGS_FILE_PATH))
+    matrix_embedding
+    return (matrix_embedding,)
+
+
+@app.cell
+def _(nn, pack_padded_sequence, torch):
+    class Model(nn.Module):
+        def __init__(self, matrix_embedding: torch.Tensor) -> None:
+            super().__init__()
+
+            self.embedding = nn.Embedding.from_pretrained(
+                matrix_embedding,
+                freeze=True,
+                padding_idx=0,
+            )
+            self.lstm = nn.LSTM(
+                self.embedding.embedding_dim,
+                hidden_size=128,
+                batch_first=True,
+                bidirectional=True,
+            )
+            self.linear = nn.Linear(
+                self.lstm.hidden_size * 2,
+                3,
+            )
+
+        def forward(
+            self, sequences: torch.Tensor, lengths: torch.Tensor
+        ) -> torch.Tensor:
+            embedded = self.embedding(sequences)
+
+            _, (hn, _) = self.lstm(
+                pack_padded_sequence(
+                    embedded, lengths, batch_first=True, enforce_sorted=False
+                )
+            )
+
+            concatenated_hn = torch.concat((hn[0], hn[1]), dim=1)
+
+            return self.linear(concatenated_hn)
+
+    return (Model,)
+
+
+@app.cell
+def _(Model, matrix_embedding, sample_batch):
+    model = Model(matrix_embedding)
+    test = model(sample_batch[0], sample_batch[2])
+    test.shape
     return
 
 
 @app.cell
-def _(DataLoader, collate, test_dataset):
-    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, collate_fn=collate)
+def _():
     return
 
 
