106af2e64f07b93d4ed278fd59e0d7fd066ca3d0
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-02 11:14:53 -0700

    refactor(dl): move model definition to separate module and add getter function

--- dl/notebooks/model.py
+++ dl/notebooks/model.py
@@ -6,27 +6,14 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import datasets
     import torch
     from torch.utils.data import DataLoader
-    from torch.nn.utils.rnn import pack_padded_sequence
-    from torch import nn
-    import datasets
     from utils.get_dataset import get_dataset
+    from utils.model import get_model
     from utils.preprocess import map_sentence
-    import numpy as np
-    from utils import constants
 
-    return (
-        DataLoader,
-        constants,
-        datasets,
-        get_dataset,
-        map_sentence,
-        nn,
-        np,
-        pack_padded_sequence,
-        torch,
-    )
+    return DataLoader, datasets, get_dataset, get_model, map_sentence, torch
 
 
 @app.cell
@@ -109,48 +96,13 @@ def _(DataLoader, collate, test_dataset, train_dataset, val_dataset):
 
 
 @app.cell
-def _(constants, np, torch):
-    matrix_embedding = torch.tensor(np.load(constants.MATRIX_EMBEDDINGS_FILE_PATH))
+def _(get_model):
+    model = get_model()
     return
 
 
 @app.cell
-def _(nn, pack_padded_sequence, torch):
-    class Model(nn.Module):
-        def __init__(self, matrix_embedding: torch.Tensor) -> None:
-            super().__init__()
-
-            self.embedding = nn.Embedding.from_pretrained(
-                matrix_embedding,
-                freeze=True,
-                padding_idx=0,
-            )
-            self.lstm = nn.LSTM(
-                self.embedding.embedding_dim,
-                hidden_size=128,
-                batch_first=True,
-                bidirectional=True,
-            )
-            self.linear = nn.Linear(
-                self.lstm.hidden_size * 2,
-                3,
-            )
-
-        def forward(
-            self, sequences: torch.Tensor, lengths: torch.Tensor
-        ) -> torch.Tensor:
-            embedded = self.embedding(sequences)
-
-            _, (hn, _) = self.lstm(
-                pack_padded_sequence(
-                    embedded, lengths, batch_first=True, enforce_sorted=False
-                )
-            )
-
-            concatenated_hn = torch.concat((hn[0], hn[1]), dim=1)
-
-            return self.linear(concatenated_hn)
-
+def _():
     return
 
 
