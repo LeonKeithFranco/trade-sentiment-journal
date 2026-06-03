880205dec790eb0a3dc9bdfd7fb4d312474dffed
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-03 14:14:49 -0700

    chore(backend): add better error message when trying to load model

--- backend/app/core/model.py
+++ backend/app/core/model.py
@@ -3,9 +3,13 @@ import asyncio
 from dl.model import get_model as get_dl_model
 from torch import nn
 
+from dl import constants
+
 
 async def get_model() -> nn.Module:
     try:
         return await asyncio.to_thread(get_dl_model)
-    except FileNotFoundError:
-        raise FileNotFoundError("Model pth file could not be loaded")
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(
+            f"{constants.MODEL_FILE_PATH} file could not be loaded; file may not yet exist"
+        ) from exc
