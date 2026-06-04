3c514b629b19e1f774001ea957964c9074299297
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-04 08:02:23 -0700

    tests(backend,nlp): add happy path test

---
+++ backend/tests/integration/test_nlp.py
@@ -0,0 +1,29 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+
+class TestAnalyze:
+    @pytest.mark.parametrize(
+        "text,sentiment",
+        (
+            ("This is some text", "neutral"),
+            ("Profit up revenue up", "positive"),
+            ("Crash loss bankruptcy", "negative"),
+        ),
+    )
+    def test_analyze(
+        self, client: TestClient, access_token: str, text: str, sentiment: str
+    ) -> None:
+        response = client.post(
+            "/analyze",
+            headers={"Authorization": f"Bearer {access_token}"},
+            json={"text": text},
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        data = response.json()
+
+        assert data["sentiment"] == sentiment
+        assert 0.5 < data["confidence"] < 1.0
