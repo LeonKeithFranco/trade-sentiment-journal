94e53b3332f363306767c008ece85811f99e9a44
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-04 07:54:02 -0700

    feat(backend,nlp): add analyze endpoint for model inference

--- backend/app/domains/nlp/route.py
+++ backend/app/domains/nlp/route.py
@@ -1,3 +1,16 @@
 from fastapi import APIRouter
 
+from app.domains.nlp.schemas import NLPRequest, NLPResponse
+from app.domains.nlp.service import NLPServiceDependency
+from app.security import CurrentUserDependency
+
 router = APIRouter(prefix="/analyze", tags=["nlp"])
+
+
+@router.post("", response_model=NLPResponse)
+async def analyze(
+    nlp_request: NLPRequest,
+    current_user: CurrentUserDependency,  # used to make sure API caller is registers user
+    nlp_service: NLPServiceDependency,
+) -> NLPResponse:
+    return await nlp_service.inference(nlp_request.text)
