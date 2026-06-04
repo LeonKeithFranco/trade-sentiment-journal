36f1631b9f36e83fc40e511933dd503c0cf171df
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-04 07:53:34 -0700

    feat(backend,nlp): add service layer

--- backend/app/domains/nlp/service.py
+++ backend/app/domains/nlp/service.py
@@ -1,10 +1,27 @@
 from typing import Annotated
 
+from dl.preprocess import process_and_map_sentence
 from fastapi import Depends
+
+from app.core.model import model_inference
+from app.domains.nlp.constants import SentimentEnum
+from app.domains.nlp.exceptions import EmptyTextError
+from app.domains.nlp.schemas import NLPResponse
 
 
 class NLPService:
-    pass
+    async def inference(self, text: str) -> NLPResponse:
+        processed = process_and_map_sentence(text)
+
+        if not processed:
+            raise EmptyTextError("Text being passed to model cannot be empty")
+
+        class_label, confidence_level = await model_inference(processed)
+
+        return NLPResponse(
+            sentiment=SentimentEnum(class_label),
+            confidence=confidence_level,
+        )
 
 
 NLPServiceDependency = Annotated[NLPService, Depends(NLPService)]
