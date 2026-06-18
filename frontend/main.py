f79fe3f8a6f1da86930abde8ad66cc1dbdec7aae
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-18 15:56:24 -0700

    add login and register forms

--- frontend/main.py
+++ frontend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from frontend!")
+import streamlit as st
+from src.core.config import get_settings
 
+st.title(get_settings().app.name)
 
-if __name__ == "__main__":
-    main()
+st.divider()
+
+st.header("Login")
+
+with st.form(key="login_form"):
+    st.text_input("Email", key="login_email")
+    st.text_input("Password", key="login_password", type="password")
+
+    st.form_submit_button("Login")
+
+st.divider()
+
+with st.form(key="register_form"):
+    st.text_input("Email", key="register_email")
+    st.text_input("Password", key="register_password", type="password")
+    st.text_input("Confirm Password", key="register_confirm_password", type="password")
+
+    st.form_submit_button("Login")
