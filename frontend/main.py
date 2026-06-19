8e2d3b7d61426b1b54f8e02bd98d77972e6c68d8
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-19 13:41:59 -0700

    refactor(frontend): move forms into fragments

--- frontend/main.py
+++ frontend/main.py
@@ -18,61 +18,69 @@ def _blank_fields_message(**kwargs) -> str | None:
     return ", ".join(f"{field}" for field in blank_fields)
 
 
-st.title(get_settings().app.name)
+@st.fragment
+def login_form_section() -> None:
+    st.divider()
 
-st.divider()
+    st.header("Login")
 
-st.header("Login")
+    with st.form(key="login_form"):
+        email = st.text_input("Email", key="login_email")
+        password = st.text_input("Password", key="login_password", type="password")
 
-with st.form(key="login_form"):
-    email = st.text_input("Email", key="login_email")
-    password = st.text_input("Password", key="login_password", type="password")
+        if st.form_submit_button("Login"):
+            blank_fields = _blank_fields_message(email=email, password=password)
 
-    if st.form_submit_button("Login"):
-        blank_fields = _blank_fields_message(email=email, password=password)
-
-        if blank_fields:
-            st.error(f"The following fields were left blank: {blank_fields}")
-            st.stop()
-
-st.divider()
-
-st.header("Register")
-
-
-with st.form(key="register_form"):
-    email = st.text_input("Email", key="register_email")
-    password = st.text_input("Password", key="register_password", type="password")
-    confirm_password = st.text_input(
-        "Confirm Password", key="register_confirm_password", type="password"
-    )
-
-    if st.form_submit_button("Register"):
-        blank_fields = _blank_fields_message(
-            email=email, password=password, confirm_password=confirm_password
-        )
-
-        if blank_fields:
-            st.error(f"The following fields were left blank: {blank_fields}")
-            st.stop()
-
-        if confirm_password != password:
-            st.error("The passwords you entered don't match.")
-            st.stop()
-
-        with APIClient() as client:
-            response = client.post_register(email=email, password=password)
-
-            if response.status_code == HTTPStatus.CREATED:
-                st.success("Registration was successful.")
+            if blank_fields:
+                st.error(f"The following fields were left blank: {blank_fields}")
                 st.stop()
 
-            match response.status_code:
-                case HTTPStatus.UNPROCESSABLE_ENTITY:
-                    err_detail = response.json()["detail"][0]
 
-                    st.error(
-                        convert_pydantic_error_to_human_readable_message(err_detail)
-                    )
-                case HTTPStatus.CONFLICT:
-                    st.error("User already exists.")
+@st.fragment
+def register_form_section() -> None:
+    st.divider()
+
+    st.header("Register")
+
+    with st.form(key="register_form"):
+        email = st.text_input("Email", key="register_email")
+        password = st.text_input("Password", key="register_password", type="password")
+        confirm_password = st.text_input(
+            "Confirm Password", key="register_confirm_password", type="password"
+        )
+
+        if st.form_submit_button("Register"):
+            blank_fields = _blank_fields_message(
+                email=email, password=password, confirm_password=confirm_password
+            )
+
+            if blank_fields:
+                st.error(f"The following fields were left blank: {blank_fields}")
+                st.stop()
+
+            if confirm_password != password:
+                st.error("The passwords you entered don't match.")
+                st.stop()
+
+            with APIClient() as client:
+                response = client.post_register(email=email, password=password)
+
+                if response.status_code == HTTPStatus.CREATED:
+                    st.success("Registration was successful.")
+                    st.stop()
+
+                match response.status_code:
+                    case HTTPStatus.UNPROCESSABLE_ENTITY:
+                        err_detail = response.json()["detail"][0]
+
+                        st.error(
+                            convert_pydantic_error_to_human_readable_message(err_detail)
+                        )
+                    case HTTPStatus.CONFLICT:
+                        st.error("User already exists.")
+
+
+st.title(get_settings().app.name)
+
+login_form_section()
+register_form_section()
