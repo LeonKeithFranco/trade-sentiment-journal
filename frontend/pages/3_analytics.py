03f6959f0c5243dd53b6ce1aa4dd87a71bb1e1ad
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-06-29 11:33:47 -0700

    feat(frontend): add table for total pnl and entry count

--- frontend/pages/3_analytics.py
+++ frontend/pages/3_analytics.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
+from typing import Any, cast
 
+import pandas as pd
 import plotly.express as px
 import streamlit as st
 from src.core.api import make_api_request
@@ -13,11 +15,13 @@ auth_check()
 
 
 def sentiment_chart() -> None:
+    st.subheader("Sentiment Chart")
+
     sentiment_response = make_api_request("GET", "/analytics/sentiment-vs-returns")
 
     match sentiment_response.status_code:
         case HTTPStatus.OK:
-            data = sentiment_response.json()
+            data = cast(list[dict[str, Any]], sentiment_response.json())
             all_sentiments = ["negative", "neutral", "positive"]
             existing = {item["sentiment"] for item in data}
 
@@ -41,9 +45,23 @@ def sentiment_chart() -> None:
                 ),
                 width="stretch",
             )
+
+            df_data = pd.DataFrame(data)
+            df_data = df_data.drop(["average_pnl"], axis=1)
+            df_data = df_data[["sentiment", "total_pnl", "entry_count"]]
+            df_data = df_data.rename(
+                columns={k: k.replace("_", " ").title() for k in data[0].keys()}
+            )
+
+            st.dataframe(df_data, width="stretch", hide_index=True)
         case _:
             st.error(sentiment_response.json())
 
 
+def confidence_chart() -> None:
+    confidence_response = make_api_request("GET", "/analytics/confidence-breakdown")
+
+
 sentiment_chart()
-# confidence_response = make_api_request("GET", "/analytics/confidence-breakdown")
+st.divider()
+confidence_chart()
