1686fa165f589514aefa661dc9a1f3ff4242bec7
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-07-02 15:37:31 -0700

    chore(backend,base.py): update doc strings

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
+    """Base class for all SQLAlchemy models."""
+
     id: Mapped[int] = mapped_column(
         primary_key=True,
     )
